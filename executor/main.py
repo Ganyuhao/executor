1d61c7ad6821fc42433ef32bc14441ef1b01ccd4
GanYuHao <498357324@qq.com>
2019-10-24 14:29:55 +0800

    config log system

--- executor/main.py
+++ executor/main.py
@@ -3,12 +3,14 @@
 """
 executor项目入口文件
 """
+import logging
 from functools import partial
 
 import click
 
-from executor.manage.config import Manage, IpAddressParamType, PortParamType
+from executor.common.config import Manage, IpAddressParamType, PortParamType
 
+LOG = logging.getLogger(__name__)
 # 将click.option转换为偏函数，强制添加两个默认参数
 OPTION = partial(click.option, show_default=True, show_envvar=True)
 
@@ -32,17 +34,29 @@ OPTION = partial(click.option, show_default=True, show_envvar=True)
 )
 @OPTION(
     "--debug",
-    required=False,
+    default=False,
     is_flag=True,
     flag_value=True,
     envvar="FUCKER_EXECUTOR_DEBUG",
     help="enable debug mode",
     type=click.BOOL
 )
+@OPTION(
+    "--log-file",
+    default="/var/log/fucker/executor.log",
+    envvar="FUCKER_EXECUTOR_LOG_FILE",
+    type=click.types.Path(),
+    help="log file path",
+)
 def main(**kwargs):
-    """程序入口，返回配置参数到单例类 Manage"""
-    Manage(**kwargs)
+    """
+    程序入口，返回配置参数到单例类 Manage, LogConfig类接收debug状态，如果
+    状态为True, 则日志系统会设置为 debug级别输入，否则默认为 INFO
+    """
+    conf = Manage(**kwargs)
+    conf.setup_log()
+    LOG.info("start server on host: %s, port :%s", conf.host, conf.port)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- executor/manage/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-# -*- encoding: utf-8 -*-
-# @Author:      Mr.Gan
-# @Time:        2019/10/21 20:33
-# @File:        __init__.py.py
